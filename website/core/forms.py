c3ab39b14812f3f2f4fa2b9bd4ec88ff10f7e3bd
Evgeniy V <volochek93@yandex.ru>
2023-04-26 15:35:52 +0400

    add form

---
+++ website/core/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+class ContactForm(forms.Form):
+    """Форма заявки."""
+
+    first_name = forms.CharField(max_length=50)
+    phone_number = forms.IntegerField(min_value=89000000000,
+                                      max_value=89999999999)
+    message = forms.CharField(widget=forms.Textarea,
+                              max_length=2000)
