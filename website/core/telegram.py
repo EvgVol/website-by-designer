c8477b7702a03c63419cb5268bbf9587bcadf55a
Evgeniy V <volochek93@yandex.ru>
2023-08-08 12:30:44 +0400

    flake8

--- website/core/telegram.py
+++ website/core/telegram.py
@@ -9,17 +9,13 @@ dp.middleware.setup(LoggingMiddleware())
 
 
 async def send_telegram_notification(order):
-    if order.email:
-        email_text = f"Email: {order.email}\n"
-    else:
-        email_text = ""
 
     message = (
         f"Новый заказ презентации\n"
         f"Имя: {order.first_name}\n"
         f"Телефон: {order.phone_number}\n"
-        f"Сообщение: {order.content}\n" +
-        email_text
+        f"Сообщение: {order.content}\n"
+        f"Email: {order.email}\n"
     )
 
     await bot.send_message(chat_id=settings.TELEGRAM_CHAT_ID, text=message)
