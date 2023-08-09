509e1f3782e12fcef8a3c489babf427c97c4a598
Evgeniy V <volochek93@yandex.ru>
2023-08-09 09:44:22 +0400

    without asyncio

--- website/core/email.py
+++ website/core/email.py
@@ -1,27 +1,21 @@
 from django.conf import settings
+from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
-import aiosmtplib
-from email.message import EmailMessage
 
 
-async def send_contact_email_message(order):
-    """Функция отправки сообщения на электронную почту."""
-    client_email = order.email if order.email else None
+def send_contact_email_message(order):
+    """Фунция отправки сообщения на электронную почту."""
+    email = order.email if order.email else None
     message = render_to_string('core/includes/order_email_send.html', {
         'first_name': order.first_name,
         'phone_number': order.phone_number,
         'content': order.content,
-        'email': client_email,
+        'email': email,
     })
-
-    email = EmailMessage()
-    email.set_content(message, "html")
-    email["Subject"] = "Новый заказ презентации"
-    email["From"] = settings.EMAIL_SERVER
-    email["To"] = [settings.EMAIL_ADMIN]
-
-    async with aiosmtplib.SMTP(settings.EMAIL_HOST,
-                               settings.EMAIL_PORT) as server:
-        await server.login(settings.EMAIL_HOST_USER,
-                           settings.EMAIL_HOST_PASSWORD)
-        await server.send_message(email)
+    email = EmailMessage(
+        'Новый заказ презентации',
+        message,
+        settings.EMAIL_SERVER,
+        [settings.EMAIL_ADMIN]
+    )
+    email.send(fail_silently=False)
--- website/core/views.py
+++ website/core/views.py
@@ -1,30 +1,15 @@
 from django.shortcuts import render
-from django.template.response import TemplateResponse
-from channels.db import database_sync_to_async
-
-import asyncio
 
 from .forms import OrderForm
 from .email import send_contact_email_message
-from .telegram import send_telegram_notification
 
 
-@database_sync_to_async
-def save_form_async(form):
-    return form.save()
-
-
-async def home(request):
+def home(request):
     if request.method == 'POST':
         form = OrderForm(request.POST)
         if form.is_valid():
-            order = await save_form_async(form)
-            tasks = [
-                send_contact_email_message(order),
-                send_telegram_notification(order),
-            ]
-            await asyncio.gather(*tasks)
-
+            order = form.save()
+            send_contact_email_message(order)
             return render(
                 request, 'core/index.html', {'form': OrderForm(),
                                              'success': True}
@@ -32,7 +17,7 @@ async def home(request):
     else:
         form = OrderForm()
 
-    return TemplateResponse(request, 'core/index.html', {'form': form})
+    return render(request, 'core/index.html', {'form': form})
 
 
 def policy(request):
