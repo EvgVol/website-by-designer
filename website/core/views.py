fe162146a07e3ea0ad45b6e6da07e628c38839fd
Evgeniy V <volochek93@yandex.ru>
2023-08-09 11:56:51 +0400

    without csrf

--- website/core/views.py
+++ website/core/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 from .forms import OrderForm
 from .email import send_contact_email_message
 
 
+@csrf_exempt
 def main(request):
     if request.method == 'POST':
         form = OrderForm(request.POST)
